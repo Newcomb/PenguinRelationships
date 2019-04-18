4ed88141114e51a2de2b8d9ffdd3447e02d4228d
RWNoel09 <47224683+RWNoel09@users.noreply.github.com>
2019-04-18 14:40:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ dataP.then(function(data){
 
 var makePlot = function(data){
     var quiz = data.map(function(d,i){return data[i].quizes.map(function(h){return h.grade})})
+    var penArray=["Bookworm","Crafty","Cyclist","Drunken","Easter","EBook","Farmer","Gentleman","Judo","Moana","Painter","Grill","Pharoh","Pilot","Pinga","Pixie","Sailor","Santa","Tauch","Tux","ValentineO","Valentine","Wizard"]
     console.log(quiz)
 
 }
